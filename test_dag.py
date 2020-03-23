756873cb09013f1a43ec1e8c16dcb3e066515e7c
Gitea <gitea@fake.local>
2020-03-23 13:43:32 -0400

    test dag

--- test_dag.py
+++ test_dag.py
@@ -5,8 +5,8 @@ from airflow.operators.dummy_operator import DummyOperator
 
 dag = DAG('TestDAG', "A dummy test DAG", schedule_interval=timedelta(minutes=5))
 
-test1 = DummyOperator(task_id="test 1", dag=dag)
-test2 = DummyOperator(task_id="test 2", dag=dag)
-test3 = DummyOperator(task_id="test 3", dag=dag)
+test1 = DummyOperator(task_id="test1", dag=dag)
+test2 = DummyOperator(task_id="test2", dag=dag)
+test3 = DummyOperator(task_id="test3", dag=dag)
 
 test1 >> test2 >> test3
