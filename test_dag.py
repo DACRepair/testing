fd452eef72c28ce3ab974dd0e1205aea9482cbb8
Gitea <gitea@fake.local>
2020-03-23 13:45:18 -0400

    test dag

--- test_dag.py
+++ test_dag.py
@@ -1,9 +1,10 @@
 from datetime import timedelta
 
+from airflow.utils.dates import days_ago
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 
-dag = DAG('TestDAG', "A dummy test DAG", schedule_interval=timedelta(minutes=5))
+dag = DAG('TestDAG', "A dummy test DAG", schedule_interval=timedelta(minutes=5), start_date=days_ago(2))
 
 test1 = DummyOperator(task_id="test1", dag=dag)
 test2 = DummyOperator(task_id="test2", dag=dag)
